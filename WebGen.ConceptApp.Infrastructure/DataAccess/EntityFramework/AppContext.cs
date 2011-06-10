960289d77b14366e6176455fd742f2455d6745eb
Thiago Alves <thiago.alves@inspira.com.br>
2011-06-10 01:01:08 -0300

    Experimenting mono

--- WebGen.ConceptApp.Infrastructure/DataAccess/EntityFramework/AppContext.cs
+++ WebGen.ConceptApp.Infrastructure/DataAccess/EntityFramework/AppContext.cs
@@ -1,20 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Data.Entity;
 using WebGen.ConceptApp.Domain.Entities;
-using WebGen.ConceptApp.Infrastructure.DataAccess;
 
 namespace WebGen.ConceptApp.Infrastructure.DataAccess.EntityFramework
 {
     public class AppContext : DbContext
     {
-        public AppContext()
-        {
-            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<AppContext>());
-        }
-
         public DbSet<Cliente> Clientes { get; set; }
     }
 }
