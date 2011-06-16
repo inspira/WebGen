db2085f8ebd353f77299c2951f0bfc01bf30a364
Thiago Alves <thiago@inspira.com.br>
2011-06-15 21:20:42 -0300

    Trying to find an Entity Framework standard

---
+++ WebGen.Infrastructure.EntityFramework/EntityFrameworkPersister.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WebGen.ApplicationServices.InversionOfControl;
+using System.Data.Entity;
+
+namespace WebGen.Infrastructure.DataAccess
+{
+    public class EntityFrameworkPersister : IOrmPersister
+    {
+        public void SaveChanges(IInversionOfControlContainer container)
+        {
+            if (System.Web.HttpContext.Current.Items.Contains("DbContext"))
+            {
+                var context = (DbContext)System.Web.HttpContext.Current.Items["DbContext"];
+                if (context != null)
+                {
+                    context.SaveChanges();
+                }
+            }
+        }
+    }
+}
---
+++ WebGen.Infrastructure.EntityFramework/IOrmPersister.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WebGen.ApplicationServices.InversionOfControl;
+
+namespace WebGen.Infrastructure.DataAccess
+{
+    public interface IOrmPersister
+    {
+        void SaveChanges(IInversionOfControlContainer container);
+    }
+}
